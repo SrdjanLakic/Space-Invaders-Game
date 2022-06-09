3112205a62705e5f53f5df77af643151b1b61e91
SrdjanLakic <srdjan949@gmail.com>
2022-06-09 17:14:55 -0400

    moveShooter function added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const grid = document.querySelector('.grid');
+let currentShooterIndex = 202;
+let width = 15;
 
 for (let i = 0; i < 225; i++) {
   const square = document.createElement('div');
@@ -21,4 +23,19 @@ function draw() {
 }
 draw();
 
-squares[200].classList.add('shooter');
+squares[currentShooterIndex].classList.add('shooter');
+
+function moveShooter(e) {
+  squares[currentShooterIndex].classList.remove('shooter');
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (currentShooterIndex % width !== 0) currentShooterIndex -= 1;
+      break;
+    case 'ArrowRight':
+      if (currentShooterIndex % width < width - 1) currentShooterIndex += 1;
+      break;
+  }
+  squares[currentShooterIndex].classList.add('shooter');
+}
+
+document.addEventListener('keydown', moveShooter);
